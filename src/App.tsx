c670c78ae69f2746c67348d6599c2f3153d429a3
chloehan <chloehan@overdare.com>
2024-05-02 17:46:58 +0900

    🎉 Set up browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
 import './App.scss'
+import { RouterProvider, createBrowserRouter, redirect, Outlet} from 'react-router-dom';
+import Intro from './pages/main/Intro/Intro'
+import DemoIntro from './pages/main/DemoIntro/DemoIntro';
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    loader: ()=>redirect('/demo'),
+  },
+  {
+    path: '/demo',
+    element: <Outlet />,
+    children: [
+      { index: true,
+        element: <Intro />,
+      },
+      {
+        path:'intro',
+        element: <DemoIntro />,
+      }
+    ],
+  },
+  // {
+  //   path:'*',
+  //   element: <PageNotFound />,
+  // }
+]);
 
 function App() {
-
-  return (
-    <>
-    <p>Hello world!</p>
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
