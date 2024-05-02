63ec2c1e71958e68755e4cdf0571edce8810cb85
chloehan <chloehan@overdare.com>
2024-05-02 17:45:24 +0900

    ✨ Add button onclick event

--- src/pages/main/Intro/Card/Card.tsx
+++ src/pages/main/Intro/Card/Card.tsx
@@ -1,7 +1,11 @@
 import styles from './Card.module.scss';
 import introImg from '../../../../public/images/intro.png';
+import { useNavigate } from 'react-router-dom';
+
 
 export default function Card(){
+  const navigate = useNavigate();
+
   return <div className={styles.card}>
     <img src={introImg} alt='intro' />
     <div className={styles.container}>
@@ -10,7 +14,7 @@ export default function Card(){
         <br />set up NFT license,
         <br />and sell your costumes here
       </p>
-      <button>ENTER Avatar Costume Shop</button>
+      <button onClick={()=>navigate('/demo/intro')}>ENTER Avatar Costume Shop</button>
     </div>
   </div>
 }
