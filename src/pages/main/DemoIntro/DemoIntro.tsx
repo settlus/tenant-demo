d0d308f21af5f7c542873a00c965cb0b75d91bc0
chloehan <chloehan@overdare.com>
2024-05-02 17:46:13 +0900

    ✨ Add navigation

--- src/pages/main/DemoIntro/DemoIntro.tsx
+++ src/pages/main/DemoIntro/DemoIntro.tsx
@@ -1,13 +1,17 @@
 import MissionCard from './MissionCard/MissionCard.tsx';
 import Mission from '../../../shared/Mission/Mission.tsx';
+import Navigation from '../../../shared/Navigation/Navigation.tsx';
 import styles from './DemoIntro.module.scss';
 
 export default function DemoIntro(){
-  return <div className={styles.intro}>
+  return <div className={styles.pos}>
+  <Navigation path='/demo' isBackwards={true}/>
+  <div className={styles.intro}>
     <p>On this Demo, you will experience as a creator and learn the value of NFT licensing on Settlus Blockchain.
        Follow the guides and complete the missions!</p>
     <Mission />
     <MissionCard />
-
+  </div>
+  <Navigation path='/demo'/>
   </div>
 }
