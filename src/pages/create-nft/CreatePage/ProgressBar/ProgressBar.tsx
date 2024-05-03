16edbe8a53635bcf7b7d432da6d078beeff62418
chloehan <chloehan@overdare.com>
2024-05-03 15:56:47 +0900

    ✨ Add Layout

---
+++ src/pages/create-nft/CreatePage/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,25 @@
+import styles from './ProgressBar.module.scss';
+
+type PropType = {
+  step: number,
+}
+
+export default function ProgressBar({step}: PropType): React.ReactElement{
+  const step2 = step===2 ? styles.active : styles.inactive;
+
+  return <div className={styles.progress}>
+    <div className={styles.step}>
+      <div className={`${styles.circle} ${styles.active}`}>
+
+      </div>
+      <label className={styles.active}>Step 1</label>
+    </div>
+    <hr className={step2}/>
+    <div className={`${styles.step} ${styles.inactive}`}>
+      <div className={`${styles.circle} ${step2}`}>
+
+      </div>
+      <label className={step2}>Step 2</label>
+    </div>
+  </div>
+}
