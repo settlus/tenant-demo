f221aa5d4f090e98b31208972339c388e8d2561f
chloehan <chloehan@overdare.com>
2024-05-03 15:52:41 +0900

    ♻️ Change variable name

--- src/shared/Navigation/Navigation.tsx
+++ src/shared/Navigation/Navigation.tsx
@@ -2,12 +2,12 @@ import styles from './Navigation.module.scss';
 import navSvg from '../../public/svg/navigation.svg';
 import {Link} from 'react-router-dom';
 
-type PathType = {
+type PropType = {
   path: string,
   isBackwards?: boolean,
 }
 
-export default function Navigation({path, isBackwards}: PathType): React.ReactElement{
+export default function Navigation({path, isBackwards}: PropType): React.ReactElement{
   return <Link to={path} className={styles.navigation}>
     <img src={navSvg} alt='navigation' className={isBackwards ? styles.backwards : ''}/>
   </Link>
