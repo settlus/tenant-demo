5823d8205520ca8a30dc4e01ef7ee5bd98d5b336
chloehan <chloehan@overdare.com>
2024-05-03 21:17:32 +0900

    ✨ Add prop

--- src/shared/Navigation/Navigation.tsx
+++ src/shared/Navigation/Navigation.tsx
@@ -1,14 +1,23 @@
 import styles from './Navigation.module.scss';
 import navSvg from '../../public/svg/navigation.svg';
-import {Link} from 'react-router-dom';
+import { useNavigate} from 'react-router-dom';
 
 type PropType = {
-  path: string,
+  path?: string,
+  handleClick?: (...args: any[]) => any,
   isBackwards?: boolean,
 }
 
-export default function Navigation({path, isBackwards}: PropType): React.ReactElement{
-  return <Link to={path} className={styles.navigation}>
-    <img src={navSvg} alt='navigation' className={isBackwards ? styles.backwards : ''}/>
-  </Link>
+export default function Navigation({path, handleClick, isBackwards}: PropType): React.ReactElement{
+  const navigate = useNavigate();
+  const handleNavigate = handleClick || function(){
+    if(path) navigate(path);
+  }
+
+  return <img src={navSvg} 
+      alt='navigation'
+      onClick={handleNavigate}
+      className={isBackwards ? styles.backwards : ''}
+      />
+
 }
