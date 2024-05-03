b01040ec5eedad79beaba301fd55f4c430a3cd47
chloehan <chloehan@overdare.com>
2024-05-03 19:05:47 +0900

    ✨ Demo root layout

---
+++ src/pages/Demo/Demo.tsx
@@ -0,0 +1,10 @@
+import Header from "../../shared/Header/Header";
+import { Outlet } from 'react-router-dom';
+import styles from './Demo.module.scss';
+
+export default function Demo(){
+  return <div className={styles.main}>
+    <Header />
+    <Outlet />
+  </div>
+}
