6bce964d61be2be250ffdc2fed9ba8d4dbaffd5e
chloehan <chloehan@overdare.com>
2024-05-03 19:02:11 +0900

    ✨ Create nft route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import './App.scss'
 import { RouterProvider, createBrowserRouter, redirect, Outlet} from 'react-router-dom';
 import Intro from './pages/main/Intro/Intro'
 import DemoIntro from './pages/main/DemoIntro/DemoIntro';
+import CreatePage from './pages/create-nft/CreatePage/CreatePage.tsx';
+import Demo from './pages/Demo/Demo.tsx';
+
 
 const router = createBrowserRouter([
   {
@@ -18,9 +21,19 @@ const router = createBrowserRouter([
       {
         path:'mission',
         element: <DemoIntro />,
-      }
+      },
     ],
   },
+  {
+    path: '/demo',
+    element: <Demo />,
+    children: [
+      {
+        path:'create-nft',
+        element: <CreatePage />,
+      }
+    ]
+  }
   // {
   //   path:'*',
   //   element: <PageNotFound />,
