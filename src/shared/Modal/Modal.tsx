5e5fe4325757499af105d43f3972052fe3838603
chloehan <chloehan@overdare.com>
2024-05-08 16:26:37 +0900

    ✨ Export modal as component

---
+++ src/shared/Modal/Modal.tsx
@@ -0,0 +1,26 @@
+import {useRef, useEffect} from 'react';
+import styles from './Modal.module.scss';
+
+type PropType={
+  children: React.ReactNode,
+  open: boolean,
+  handleClose: ()=>void,
+}
+
+export default function Modal({children, open, handleClose}: PropType): React.ReactElement{
+  const dialog=useRef<HTMLDialogElement>(null);
+  
+  useEffect(()=>{
+    if(dialog.current){
+      if(open) dialog.current.showModal();
+      else dialog.current.close();
+    }
+  },[open]);
+
+  return <dialog ref={dialog} className={styles.dialog}>
+    <div className={styles.content}>
+    <button className={styles.close} onClick={handleClose}>x</button>
+    {children}
+    </div>
+  </dialog>
+}
