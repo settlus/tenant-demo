79ee70f15f43fd6dcfda1204c46abd9ea2bcdbf8
chloehan <chloehan@overdare.com>
2024-05-08 16:28:03 +0900

    ✨ Add Prop

--- src/pages/create-nft/CreatePage/Submit/Submit.tsx
+++ src/pages/create-nft/CreatePage/Submit/Submit.tsx
@@ -2,11 +2,19 @@ import styles from './Submit.module.scss';
 import QnaBanner from './QnaBanner/QnaBanner';
 import Field from './Field/Field';
 
-export default function Submit (){
+type PropType = {
+  info: {
+    price: string | null,
+    name: string | null,
+  },
+  handleInfo: (field: string, value: string )=>void,
+}
+
+export default function Submit ({info, handleInfo}: PropType): React.ReactElement{
   return <div className={styles.main}>
     <form>
-      <Field label='Item Title' />
-      <Field label='Item Price($)' />
+      <Field label='Item Title' value={info.name || ''} handleInfo={handleInfo}/>
+      <Field label='Item Price($)' value={info.price || ''} handleInfo={handleInfo}/>
     </form>
 
     <QnaBanner />
