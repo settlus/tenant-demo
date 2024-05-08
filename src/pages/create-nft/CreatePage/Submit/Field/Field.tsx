5c4a2bf1e74e718eea53d0f6b234a778df3b1937
chloehan <chloehan@overdare.com>
2024-05-08 16:26:53 +0900

    ✨ Add handleInfo prop

--- src/pages/create-nft/CreatePage/Submit/Field/Field.tsx
+++ src/pages/create-nft/CreatePage/Submit/Field/Field.tsx
@@ -1,12 +1,22 @@
+import { ChangeEvent } from 'react';
 import styles from './Field.module.scss';
 
 type PropType = {
   label: string, 
+  handleInfo: (field: string, value: string)=>void,
+  value: string,
 }
 
-export default function Field({label}: PropType): React.ReactElement{
+export default function Field({value, label, handleInfo}: PropType): React.ReactElement{
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>){
+    if(label==='Item Title') handleInfo('name',e.target.value);
+    else handleInfo('price',e.target.value);
+    
+  }
+
   return <div className={styles.field}>
     <label>{label}</label>
-    <input type='text' required/> 
+    <input type='text' required value={value} onChange={handleChange}/> 
   </div>
 }
