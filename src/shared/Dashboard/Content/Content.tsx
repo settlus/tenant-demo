389a731618630cf53483a858c5c8384535bfe0d7
chloehan <chloehan@overdare.com>
2024-05-10 13:32:21 +0900

    ✨ Display data

--- src/shared/Dashboard/Content/Content.tsx
+++ src/shared/Dashboard/Content/Content.tsx
@@ -1,17 +1,61 @@
 import styles from './Content.module.scss';
 import Card from '../Card/Card.tsx';
 
-export default function Content(){
+type PropType={
+  type: string,
+  data: {
+    image: string,
+    table: any,
+    card: any,
+  } | null,
+}
+
+
+export default function Content({type,data}: PropType): React.ReactElement{
+  const DATA = data || {
+    image: '',
+    table:  [{}],
+    card: '',
+  }
+
+  const KEYS = Object.keys(DATA.table[0]);
+
   return  <div className={styles.main}>
     <div className={styles.table1}>
-      <label>NFTs</label>
-      <span></span>
+      <label>{type==='on-chain' ? 'NFTs' : 'Items'}</label>
+      <span>
+        <img src={DATA.image} />
+      </span>
     </div>
     <div className={styles.table2}>
-      <label>History</label>
-      <span></span>
+      <label>{type==='on-chain' ? 'Activity History' : 'Revenue Table'}</label>
+      <span>
+        <table>
+          <thead>  
+          <tr>
+            <th>Thumbnail</th>
+            {KEYS.map((item,index)=>(
+              <th key={index}>{item}</th>
+            ))}
+          </tr>
+          </thead>
+          <tbody>
+            {DATA.table.map((item: any,index: any) => (
+              <tr key={index}>
+                <td><img src={DATA.image}/></td>
+                {KEYS.map((item1,index1)=>(
+                  <td key={index1}>
+                    {item1==='Txn Hash' ? <a href={`https://scan.migaloo.io/tx/${item[item1]}`}>{item[item1]}</a> : item[item1]}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+          
+        </table>
+      </span>
     </div>
-    <Card />
+    <Card image={DATA.image} content={type==='on-chain' ? DATA.card : DATA.table}/>
   </div> 
   
 }
