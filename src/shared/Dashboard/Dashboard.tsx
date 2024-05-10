c240d1bab6c3322ba86b1f2e950dd992bb3ef2ec
chloehan <chloehan@overdare.com>
2024-05-10 13:30:07 +0900

    ✨ Load & Pass data through props

--- src/shared/Dashboard/Dashboard.tsx
+++ src/shared/Dashboard/Dashboard.tsx
@@ -1,24 +1,32 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import styles from './Dashboard.module.scss';
 import Header from './Header/Header';
 import Menu from './Menu/Menu';
 import Content from './Content/Content';
+import { getData } from '../../apis/api';
 
 export default function Dashboard(){
-  const [type,setType]=useState<string>('on-chain');
+  const [type,setType]=useState('on-chain');
+  const [data,setData]=useState({'on-chain': null, 'off-chain': null});
+
+  useEffect(()=>{
+    const setMainData = async ()=>{
+      const info = await getData(type);
+      setData(info);
+    }
+
+    setMainData();
+  },[]);
 
   function handleClick(type: string){
     if(type==='on') setType('on-chain');
     else setType('off-chain');
-    
   }
  
   return <div className={styles.board}>
     <Header />
     <Menu handleClick={handleClick}/>
-
-    {type==='on-chain' && <Content />}
-    {type==='off-chain' && <Content />}
+    <Content type={type} data={type==='on-chain' ? data['on-chain'] : data['off-chain']} />
 
   </div>
 }
