56b6b7a087490464fea99ab2c3f0f2b4c3c1d8fc
chloehan <chloehan@overdare.com>
2024-05-13 11:32:47 +0900

    ✨ Disable button on max limit

--- src/shared/Dashboard/Header/Header.tsx
+++ src/shared/Dashboard/Header/Header.tsx
@@ -2,10 +2,14 @@ import styles from './Header.module.scss';
 import {Link} from 'react-router-dom';
 import smileSvg from '../../../public/svg/smile.svg';
 
-export default function Header(){
+type PropType = {
+  length: number,
+}
+
+export default function Header({length}: PropType): React.ReactElement{
   return <header className={styles.header}>
     <img src={smileSvg} />
     <h3>Creator Dashboard</h3>
-    <Link to={'/demo/create-nft'} className={styles.link}>+ Create costume</Link>
+    <Link to={'/demo/create-nft'} className={`${styles.link} ${length>2 ? styles.disabled : ''}`} >+ Create costume</Link>
   </header>
 }
