5e20492f9a2c5f063affe11538284a5b589b220e
chloehan <chloehan@overdare.com>
2024-05-13 17:58:08 +0900

    ✨Add global provider

--- src/shared/Dashboard/Dashboard.tsx
+++ src/shared/Dashboard/Dashboard.tsx
@@ -1,13 +1,23 @@
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useContext} from 'react';
 import styles from './Dashboard.module.scss';
 import Header from './Header/Header';
 import Menu from './Menu/Menu';
 import Content from './Content/Content';
+import DashboardProvider, { DashboardContext } from '../../store/dashboard_context';
+import UserModal from './UserModal/UserModal';
 import { getData } from '../../apis/api';
 
-export default function Dashboard(){
+export default function DashboardPage(){
+  return <DashboardProvider>
+    <Dashboard />
+  </DashboardProvider>
+}
+
+function Dashboard(){
   const [type,setType]=useState('on-chain');
   const [data,setData]=useState({'on-chain': [], 'off-chain': []});
+  const {isModal} = useContext(DashboardContext);
+  console.log(isModal);
 
   useEffect(()=>{
     const setMainData = async ()=>{
@@ -24,11 +34,11 @@ export default function Dashboard(){
   }
  
   return <div className={styles.board}>
-    <Header length={data['on-chain'].length}/>
-    <Menu handleClick={handleClick}/>
-    {type==='on-chain' && <Content type={'on-chain'} data={data['on-chain']} />}
-    {type==='off-chain' && <Content type={'off-chain'} data={data['off-chain']} />}
-    {/* <Content type={type} data={type==='on-chain' ? data['on-chain'] : data['off-chain']} /> */}
-
-  </div>
+      {isModal && type==='off-chain' && <UserModal />}
+      <Header length={data['on-chain'].length}/>
+      <Menu handleClick={handleClick}/>
+      {type==='on-chain' && <Content type={'on-chain'} data={data['on-chain']} />}
+      {type==='off-chain' && <Content type={'off-chain'} data={data['off-chain']} />}
+      {/* <Content type={type} data={type==='on-chain' ? data['on-chain'] : data['off-chain']} /> */}
+    </div>
 }
