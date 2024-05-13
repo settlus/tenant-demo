8913fa83f2080aa0d1385f8573da392e9f4cfa62
chloehan <chloehan@overdare.com>
2024-05-13 17:58:53 +0900

    ✨Modal open logic

--- src/shared/Dashboard/Card/Card.tsx
+++ src/shared/Dashboard/Card/Card.tsx
@@ -1,7 +1,8 @@
 import styles from './Card.module.scss';
 import Front from './Front/Front';
 import Back from './Back/Back';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
+import { DashboardContext } from '../../../store/dashboard_context';
 
 type PropType={
   content: any,
@@ -9,7 +10,13 @@ type PropType={
 
 export default function Card({content}: PropType): React.ReactElement{
   const [state,setState] = useState({flipped: false});
+  const {setIsModal} = useContext(DashboardContext);
   const isOffChain = content && content.revenue;
+
+  const handleClick=()=>{
+    flip();
+    if(isOffChain) setIsModal(true);
+  }
   
   const flip = ()=>{
     setState((prev)=>
@@ -19,6 +26,6 @@ export default function Card({content}: PropType): React.ReactElement{
   return <div className={`${styles.card} ${state.flipped ? styles.flipped : ""}`}>
     <Back content={content}/>
     <Front image={content && content.image} price={isOffChain ? content.revenue['Price'] : null}/>
-    <button onClick={flip}>{'>'}</button>
+    <button onClick={handleClick}>{'>'}</button>
   </div>
 }
