e0afccf71a53bcad16f9eac60b699254d790a00b
chloehan <chloehan@overdare.com>
2024-05-13 15:46:22 +0900

    ✨ Display Hover message

--- src/shared/Dashboard/Card/Back/Back.tsx
+++ src/shared/Dashboard/Card/Back/Back.tsx
@@ -1,17 +1,31 @@
 import styles from '../Card.module.scss';
 import NFTLicenseMark from '../../../../public/images/NftLicense.png';
 import CoinImg from '../../../../public/images/coin.png';
+import HoverMessage from '../../HoverMessage/HoverMessage';
+import { useState } from 'react';
+
 type BackType={
   content: any,
 }
 
 
 export default function Back({content}: BackType): React.ReactElement{
+  const [isHover, setIsHover] = useState(false);
+
+  function handleEnter(){
+    setIsHover(true);
+  }
+
+  function handleLeave(){
+    setIsHover(false);
+  }
+
   return <div className={styles.back}>
     {content && content.revenue ?
     //Off-Chain Card 
     <div className={styles.offChain}>
-      <img src={NFTLicenseMark} alt={'mark'} className={styles.mark}/>
+      <HoverMessage isVisible={isHover}/>
+      <img src={NFTLicenseMark} alt={'mark'} className={styles.mark} onMouseEnter={handleEnter} onMouseLeave={handleLeave}/>
       <div className={styles.stats}>
         <div className={styles.price}>
           <label>Price($)</label>
