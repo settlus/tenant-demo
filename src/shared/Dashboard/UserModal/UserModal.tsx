4533c8018dfa6c3320ac1ac05b5505a30242e32b
chloehan <chloehan@overdare.com>
2024-05-14 16:30:37 +0900

    ✨ Add user action logic

--- src/shared/Dashboard/UserModal/UserModal.tsx
+++ src/shared/Dashboard/UserModal/UserModal.tsx
@@ -4,26 +4,58 @@ import { DashboardContext } from '../../../store/dashboard_context';
 
 type PropType={
   collapsed: boolean,
+  content: any,
 }
 
-export default function UserModal({collapsed}: PropType): React.ReactElement{
-  const {setIsModal} = useContext(DashboardContext);
+export default function UserModal({collapsed, content}: PropType): React.ReactElement{
+  const {setIsModal, selected, setSelected, data, setData} = useContext(DashboardContext);
+  const len = data['on-chain'].length;
 
   function handleClick(value: number){
     setIsModal(value);
   }
+
+  function handleNavigate(type: string){
+    if(type==='left'){
+      setSelected((prev:number)=>{
+        const updated = prev-1;
+        if(updated<0) return len-1; else return updated;
+      })
+    }
+
+    if(type==='right'){
+      setSelected((prev:number)=>{
+        const updated = prev+1;
+        if(updated>=len) return 0; else return updated;
+      })
+    }
+
+  }
+
+  function handleBuy(){
+    setData((prev: any)=>{
+      let updated={...prev,}
+      const selectedItem = {...updated['off-chain'][selected]};
+      
+      selectedItem.revenue['Quantity'] += 1;
+      selectedItem.revenue['Total Sales Revenue'] += selectedItem.revenue['Price'];
+
+      updated['off-chain'][selected] = selectedItem;
+      return updated;
+    })
+  } 
 
   return <>
   {!collapsed && <div className={`${styles.modal}`}>
         <button className={styles.close} onClick={()=>{handleClick(1)}}>-</button>
         <h3>USER MODE</h3>
         <div className={styles.main}>
-          <button>{'<'}</button>
+          <button onClick={()=>handleNavigate('left')}>{'<'}</button>
           <div className={styles.card}>
-            <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/Lake_mapourika_NZ.jpeg/1280px-Lake_mapourika_NZ.jpeg'/>
-            <button>BUY</button>
+            <img src={content.image}/>
+            <button onClick={handleBuy}>BUY</button>
           </div>
-          <button>{'>'}</button>
+          <button onClick={()=>handleNavigate('right')}>{'>'}</button>
         </div>
         <p>On the Avatar Costume Shop, your NFT-licensed item is listed as shown above. 
           Click to buy the item.</p>
