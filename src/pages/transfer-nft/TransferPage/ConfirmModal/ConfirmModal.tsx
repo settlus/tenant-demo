f640c555a075ff4e6d4238ec382323bbee758b77
chloehan <chloehan@overdare.com>
2024-05-16 17:35:55 +0900

    ✨Transfer confirmation modal

---
+++ src/pages/transfer-nft/TransferPage/ConfirmModal/ConfirmModal.tsx
@@ -0,0 +1,35 @@
+import Modal from "../../../../shared/Modal/Modal";
+import sendIcon from "../../../../public/images/send.png";
+import styles from "./ConfirmModal.module.scss";
+import { transferNFT } from "../../../../apis/api";
+import {useNavigate} from "react-router-dom";
+
+type PropType={
+  handleClose: ()=>void,
+  open: boolean, 
+  offer: any,
+  data: any,
+}
+
+export default function ConfirmModal({open, handleClose, offer, data}:PropType):React.ReactElement{
+  const navigate = useNavigate();
+
+  async function handleConfirm(){
+    await transferNFT(offer, data);
+    navigate('/demo/complete');
+  }
+
+  return <Modal open={open} handleClose={handleClose} style={styles.style}>
+    <div className={styles.main}>
+      <h2>Confirm</h2>
+      <img src={sendIcon} />
+      <p>
+      Once transferred, profits from NFT-licensed item will be vested to the new owner.
+      <br /><br />
+      You will receive ${offer.offerPrice} to your account.<br />
+      (In demo, no payment will be received.)
+      </p>
+      <button onClick={handleConfirm}>Confirm</button>
+    </div>
+  </Modal>
+}
--- src/pages/transfer-nft/TransferPage/TransferPage.tsx
+++ src/pages/transfer-nft/TransferPage/TransferPage.tsx
@@ -1,9 +1,11 @@
 import styles from './TransferPage.module.scss';
 import docImg from '../../../public/images/doc.png';
-import { useContext } from 'react';
+import ConfirmModal from './ConfirmModal/ConfirmModal';
+import { useContext, useState } from 'react';
 import { DashboardContext } from '../../../store/dashboard_context';
 
 export default function TransferPage(){
+  const [isModal, setIsModal] = useState(false);
   const {offer, data} = useContext(DashboardContext);
   const index= offer ? offer.itemIndex : 0;
 
@@ -12,7 +14,16 @@ export default function TransferPage(){
     title: data['on-chain'][index].title,
   }
 
+  function handleOpen(){
+    setIsModal(true);
+  }
+
+  function handleClose(){
+    setIsModal(false);
+  }
+
   return <div className={styles.main}>
+    {isModal && <ConfirmModal open={isModal} handleClose={handleClose} offer={offer} data={data}/>}
     <h2>Sell NFT</h2>
     <div className={styles.image}>
       <img src={docImg} className={styles.icon}/>
@@ -20,6 +31,6 @@ export default function TransferPage(){
     </div>
     <p>Transfer "{INFO.title}" To: </p>
     <p>{offer?.offerAddress}</p>
-    <button>Transfer & Receive ${offer?.offerPrice}</button>
+    <button className={styles.button} onClick={handleOpen}>Transfer & Receive ${offer?.offerPrice}</button>
   </div>
 }
