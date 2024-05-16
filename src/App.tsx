23a2907457caa96519355d25bf64caa7f865406d
chloehan <chloehan@overdare.com>
2024-05-16 15:27:20 +0900

    ✨ Add router

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Intro from './pages/main/Intro/Intro'
 import DemoIntro from './pages/main/DemoIntro/DemoIntro';
 import CreatePage from './pages/create-nft/CreatePage/CreatePage.tsx';
 import Demo from './pages/demo/Demo/Demo.tsx';
-import Dashboard from './shared/Dashboard/Dashboard.tsx';
+import Main from './pages/dashboard/Main/Main.tsx';
 
 
 const router = createBrowserRouter([
@@ -37,8 +37,20 @@ const router = createBrowserRouter([
         path:'dashboard',
         element: <Outlet />,
         children: [
-          {index: true, element: <Dashboard />},
+          {index: true, element: <Main />},
+          {
+            path:'explore',
+            element:<></>,
+          },
+          {
+            path:'sell-nft',
+            element:<></>,
+          }
         ]
+      },
+      {
+        path:'transfer-nft',
+        element: <></>,
       },
     ]
   }
