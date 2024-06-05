b014c2e904b14a070920bb68590f9b3dc2a5a32e
chloehan <chloehan@overdare.com>
2024-06-05 16:00:47 +0900

    🐛 Fix bug

--- src/pages/costume-shop/Costumepage/CostumePage.tsx
+++ src/pages/costume-shop/Costumepage/CostumePage.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react"
+import { useContext, useEffect, useState } from "react"
 import Instruction from "../../../shared/Instruction/Instruction"
 import CostumeShop from "./CostumeShop/CostumeShop"
 import { ShopContext, ITEM_ARR } from "../../../store/costumeshop_context"
@@ -14,11 +14,11 @@ const TITLES = [
   'Welcome to the Avatar Costume Shop!',
   'What’s so special? These costumes are NFT licensed!',
   'Take a peak at the creator’s revenue!',
-  'Congratulations! Your item is listed for sale.',
 ]
 
 export default function CostumePage(){
   const {step, setStep, items, setItems, selected, setSelected} = useContext(ShopContext);
+  const [isAfterItemCreated, setIsAfterItemCreated] = useState(false);
   const selectedItem = items[selected];
   const navigate = useNavigate();
   const location = useLocation();
@@ -28,7 +28,7 @@ export default function CostumePage(){
       sessionStorage.setItem('mission','1');
       navigate('/demo/create-nft'); 
     }
-    setStep(prev=>prev+1);
+    else setStep(prev=>prev+1);
   }
 
   useEffect(()=>{
@@ -51,7 +51,7 @@ export default function CostumePage(){
             userCreated: true,
           }, ...prev];
         });
-        setStep(3);
+        setIsAfterItemCreated(true);
       }
       
       handleData();
@@ -68,16 +68,19 @@ export default function CostumePage(){
 
   return <div>
     <div className={styles.instruction}>
-      <Instruction title={TITLES[step]} style={styles.detail}>
-        {step===0 && <p>People who want to decorate avatars in the metaverse buy avatar costumes created by our creators. You can see the online users’ real-time shopping activities on the “Live” section.</p>}
-        {step===1 && <div className={styles.text}>
-            <p><img src={nftIcon} alt='license' /> mark shows that the costumes are minted as NFTs. The NFT guarantees the ownership of a creator's costumes, and copies of costumes can be sold as items without quantity limit on the off-chain Avatar Costume Shop. Click your favorite costume to see the detail!</p>      
-          </div>}
-        {step===2 && <p>This costume’s creator earned {formatNum(selectedItem.price*selectedItem.quantity)} for item sales and now consider selling NFT for {formatNum(selectedItem.offerValue)} on the NFT Marketplace. Once the NFT is sold, the new owner will earn profits from the item sales. That’s why NFT licensing is attractive.</p> }
-        {step===3 && <p>Now, you are one of the creators in the Avatar Costume Shop. Look at the “Live” section. Users are buying your item.
-          Click the “Go to my dashboard” button to manage your sales.</p>}
+      <Instruction title={!isAfterItemCreated ? TITLES[step]: 'Congratulations! Your item is listed for sale.'} style={styles.detail}>
+        { !isAfterItemCreated ? <>
+          {step===0 && <p>People who want to decorate avatars in the metaverse buy avatar costumes created by our creators. You can see the online users’ real-time shopping activities on the “Live” section.</p>}
+          {step===1 && <div className={styles.text}>
+              <p><img src={nftIcon} alt='license' /> mark shows that the costumes are minted as NFTs. The NFT guarantees the ownership of a creator's costumes, and copies of costumes can be sold as items without quantity limit on the off-chain Avatar Costume Shop. Click your favorite costume to see the detail!</p>      
+            </div>}
+          {step===2 && <p>This costume’s creator earned {formatNum(selectedItem.price*selectedItem.quantity)} for item sales and now consider selling NFT for {formatNum(selectedItem.offerValue)} on the NFT Marketplace. Once the NFT is sold, the new owner will earn profits from the item sales. That’s why NFT licensing is attractive.</p>}
+          <Navigation handleClick={handleNav}/>
+        </>: 
+          <p>Now, you are one of the creators in the Avatar Costume Shop. Look at the “Live” section. Users are buying your item.
+          Click the “Go to my dashboard” button to manage your sales.</p>
+        }
       </Instruction>
-      {step<3 && <Navigation handleClick={handleNav}/>}
     </div>
 
     <CostumeShop />
