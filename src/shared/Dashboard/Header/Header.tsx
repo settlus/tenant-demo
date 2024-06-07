1d4332bb61144900d788c4c095b20b8db019a986
chloehan <chloehan@overdare.com>
2024-06-07 14:26:03 +0900

    🌈 Add user profile icon

--- src/shared/Dashboard/Header/Header.tsx
+++ src/shared/Dashboard/Header/Header.tsx
@@ -1,5 +1,5 @@
 import styles from './Header.module.scss';
-import smileSvg from '../../../public/svg/smile.svg';
+import userProfile from '../../../public/svg/userProfile/userProfile.svg';
 import infoSvg from '../../../public/svg/Info.svg';
 import { getNickName } from '../../../apis/api';
 import { useRef } from 'react';
@@ -9,7 +9,7 @@ export default function Header(){
 
   return <header className={styles.header}>
     <span>
-      <img src={smileSvg} />
+      <img src={userProfile} />
       <h3>{nickname.current}’s Dashboard</h3>
     </span>
     <span>
